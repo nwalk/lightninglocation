ce16250cbfb756583015f65c492e89c1c103ee51
nrwalk8939 <nwalk19@gmail.com>
2014-10-09 16:53:48 -0400

    Created map.html, MapView, url, and updated base.html
    I created a map.html, and use base to include the navbar.
    I believe the urls are looking like thy should now, with the map url added.
    I created a map view to load the map.html
    Base.html was confusing to me, but I believe I have things worked out. Additions should work by following the "block" pattern laid out.

--- apps/locator/urls.py
+++ apps/locator/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from apps.locator import views
+from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gisc2.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^main', views.MainView.as_view(), name='main'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.MainView.as_view()),
+    url(r'^map$', views.MapView.as_view()),
 )
--- apps/locator/views.py
+++ apps/locator/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+##from django.shortcuts import render
 from django.views import generic
 
-# Create your views here.
+
 class MainView(generic.TemplateView):
     """Loads the main page."""
     template_name = 'locator/main.html'
+
+
+class MapView(generic.TemplateView):
+    """Loads the map page"""
+    template_name = 'locator/map.html'
